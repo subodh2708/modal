c3fe4973589238c8babf9804de07e1c6509bfc49
Subodh <subodhlst@gmail.com>
2025-08-07 05:07:37 +0530

    feat:add modal/overlay  display and hide functionality

--- script.js
+++ script.js
@@ -3,4 +3,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnShowModal = document.querySelector('.show-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
