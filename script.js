7c029272de984c406ddc3bc5b2aef553f10331b2
Subodh <subodhlst@gmail.com>
2025-08-07 04:57:14 +0530

    select elements and assign to variables

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelector('.show-modal');
